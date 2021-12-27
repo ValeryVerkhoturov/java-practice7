f216a0ef46fa9d19be63a641d3335d91ec8cb271
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2021-12-27 16:11:28 +0300

    Add ResponseBehavior

--- src/task2-3/server/com/company/ServerSession.java
+++ src/task2-3/server/com/company/ServerSession.java
@@ -4,31 +4,38 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.concurrent.TimeUnit;
 
-@FieldDefaults(level = AccessLevel.PRIVATE)
-public class ServerSession implements Runnable {
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ServerSession implements Runnable, Closeable {
 
-    @NonFinal
-    @Getter(onMethod_ = {@Synchronized}) @Setter(onMethod_ = {@Synchronized})
-    String name = "Unnamed";
-
-    ChatHistory chatHistory;
+    Socket socket;
 
     ObjectInputStream objectInputStream;
+
+    @Getter
     ObjectOutputStream objectOutputStream;
+
+    @NonFinal
+    @Setter @Getter
+    boolean sessionLoop = true;
+
+    @NonFinal
+    @Getter(onMethod_ = @Synchronized) @Setter(onMethod_ = @Synchronized)
+    String name = "Unnamed";
+
+    @Getter
+    ChatHistory chatHistory;
 
     @SneakyThrows
     public ServerSession(Socket socket, ChatHistory chatHistory) {
+        this.socket = socket;
         this.chatHistory = chatHistory;
 
-        // Если поменять вызов outputStream и inputStream местами,
+        // Если поменять вызов конструкторов outputStream и inputStream местами,
         // программа застрянет в конструкторе ObjectInputStream.
         // Задокументировано здесь https://docs.oracle.com/javase/6/docs/api/java/io/ObjectInputStream.html#ObjectInputStream%28java.io.InputStream%29
         objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
@@ -38,69 +45,29 @@ public class ServerSession implements Runnable {
     @SneakyThrows
     @Override
     public void run() {
-        TimeUnit.SECONDS.sleep(10);
-        objectOutputStream.writeObject(MessageDTO.builder()
-                .message("Добро пожаловать на сервер Шизофрения. Введите имя.")
-                .command(Command.OK)
-                .build());
-        objectOutputStream.flush();
+        ResponseBehavior.greetings(this);
 
-        boolean sessionLoop = true;
         while (sessionLoop) {
-            MessageDTO message = null;
             try {
-                message = (MessageDTO) objectInputStream.readObject();
+                MessageDTO message = (MessageDTO) objectInputStream.readObject();
+                switch (message.getCommand()) {
+                    case SET_NAME -> ResponseBehavior.setName(this, message);
+                    case SEND_CAT -> ResponseBehavior.sendCat(this, message);
+                    case SEND_MESSAGE -> ResponseBehavior.sendMessage(this, message);
+                    case CLOSE_SESSION -> ResponseBehavior.closeSession(this, message);
+                    case GET_LAST_MESSAGES -> ResponseBehavior.getLastMessages(this, message);
+                    default -> ResponseBehavior.error(this);
+                }
             } catch (SocketException e) {
                 break;
             }
-            switch (message.getCommand()) {
-                case SET_NAME -> {
-                    setName(message.getMessage());
-                    chatHistory.addMessage(MessageDTO.builder()
-                            .sender("@server")
-                            .recipient(message.getSender())
-                            .message("Имя установлено.")
-                            .command(Command.OK)
-                            .build());
-                }
-                case SEND_CAT ->
-                    chatHistory
-                            .addMessage(message)
-                            .addMessage(MessageDTO.builder()
-                                    .sender("@server")
-                                    .recipient(message.getSender())
-                                    .message("Кошка брошена в " + message.getRecipient())
-                                    .command(Command.OK)
-                                    .build());
-                case SEND_MESSAGE ->
-                    chatHistory
-                            .addMessage(message)
-                            .addMessage(MessageDTO.builder()
-                                    .sender("@server")
-                                    .recipient(message.getSender())
-                                    .message("Сообщение отправлено")
-                                    .command(Command.OK)
-                                    .build());
-                case CLOSE_SESSION -> {
-                    sessionLoop = false;
-                    chatHistory.addMessage(MessageDTO.builder()
-                            .sender("@server")
-                            .recipient(message.getSender())
-                            .message("Сессия закрыта")
-                            .command(Command.CLOSE_SESSION)
-                            .build());
-                }
-                case GET_LAST_MESSAGES ->
-                    objectOutputStream.writeObject(MessageDTO.builder()
-                            .sender("@server")
-                            .recipient(message.getSender())
-                            .messages(chatHistory.getLastMessages(message))
-                            .command(Command.SEND_LAST_MESSAGES)
-                            .build());
-                default -> objectOutputStream.writeObject(MessageDTO.builder()
-                        .command(Command.ERROR)
-                        .build());
-            }
         }
+
+        close();
+    }
+
+    @Override
+    public void close() throws IOException {
+        socket.close();
     }
 }
